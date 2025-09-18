43627f5ec36034f291ad2b9bfd3fead180072640
0Dema0 <lorenzodemaria2001@gmail.com>
2025-09-18 12:54:07 +0200

    First revision: supposedly correct ODE

--- Lab2.m
+++ Lab2.m
@@ -5,7 +5,7 @@ Delta_G = -3.5; % Standard Gibbs free energy, in kcal/mol
 K = exp(-Delta_G * 4184 / (R * T)); % Equilibrium constant
 fprintf('The equilibrium constant K is: %.4f\n', K);
 % Comment: since K >> 1, it means that inside the cell we have a lot more
-% substrate than prduct, and the cell is strongly drawn to the reaction (it look for equilibrium)
+% substrate than product, and the cell is strongly drawn to the reaction (it looks for equilibrium)
 
 %% Enzyme simulation
 K_m = 8e-3; % Michaleis constant, in M (or mol/liter)
@@ -26,20 +26,30 @@ for i = 1:length(X)
     ylabel('Reaction Rate (j)');
     title('Reaction Rate vs Concentration of F6P for different concentration of FBP');
     xlabel('S [M]');
-    ylabel('j/V_f []');
+    ylabel('j/V_f');
     hold on;
 end
-legend;
+legend(arrayfun(@(x) sprintf('X=%.1e', x), X, 'UniformOutput', false));
 hold off;
 
 %% Stochiometry
 N = [1,-1,0;0,1,-1];
 
-S0 = [1e-3;1e-9];
+S0 = [1e-3;1e-6];
+tspan = 0:0.1:2000;
 
-[t,y] = ode15s(@fun,[0,2000],S0);
-figure;
-plot(t,y(:,2),'-o');
+options = odeset('RelTol',1e-8,'AbsTol',1e-12,'NonNegative',1:2);
+j_in_values = [0.6e-6, 6e-6];
+for k = 1:length(j_in_values)
+    j_in = j_in_values(k);
+    [t,y] = ode15s(@(t,S) fun(t,S,j_in), [0,2000], S0, options);
+    figure;
+    plot(t, y(:,1), '-o', t, y(:,2), '-x');
+    legend('S1 (F6P)', 'S2 (FBP)', 'Location', 'best');
+    xlabel('Time (s)');
+    ylabel('Concentration (M)');
+    title(sprintf('Dynamics with j_{in} = %.1e M/s', j_in));
+end
 
 
 %% Functions
@@ -53,7 +63,7 @@ function j = Hill(K_m,K_x,K_eq,n,V_f,S,X)
 %   X        - Concentration of FBP [M]
 %
 % Output:
-%   j        - Reaction rate []
+%   j        - Reaction rate [M/s]
 
     sigma_eq = K_eq/K_m;
     alpha = sigma_eq^(-n) - 1;
@@ -63,39 +73,21 @@ function j = Hill(K_m,K_x,K_eq,n,V_f,S,X)
     j = V_f * ((sigma.^n) ./ (sigma.^n + ((1+csi^n)/(1+alpha*csi^n))));
 end
 
-function dSdt = fun(t,S)
+function dSdt = fun(t,S,j_in)
     N = [1,-1,0;0,1,-1];
 
-    j_in = 6e-6;
-
-    K_m = 8e-3;
+    K_m = 8e-3; % Michaleis constant, in M
     K_x = 3e-6; % Dissociation constant, in M (or mol/liter)
     K_eq = 0.8e-3;
     n = 2.5; % Hill coefficient
     V_f = 1.8e-3; % Limiting rate, in M/s (mol/(liter*s))
-    j_r = Hill(K_m,K_x,K_eq,n,V_f,S(1),S(2));
+    j_r = Hill(K_m,K_x,K_eq,n,V_f,S(1),S(2)); % Flux between, in M/s
 
-    V = 60e-6;
-    K_m = 10e-6;
-    j_out = V * (S(2)/(K_m+S(2)));
+    V = 60e-6; % Limiting rate, in M/s
+    K_m = 10e-6; % Michaleis constant, in M
+    j_out = V * (S(2)/(K_m+S(2))); % Flux out, in M/s
 
     j = [j_in;j_r;j_out];
 
     dSdt = N*j;
 end
-
-
-% T = 0:1:2000;
-% S = zeros(size(T));
-% X = zeros(size(T));
-% j_in = zeros(size(T));
-% j = zeros(size(T));
-% j_out = zeros(size(T));
-% 
-% S(1) = 1e-3;
-% X(1) = 1e-6;
-% j_in(1) = 0.6e-6;
-% j(1) = Hill(S(1),X(1));
-% V = 60e-6;
-% K_m = 10e-6;
-% j_out(1) = V * (X(1)/(K_m+X(1)));
